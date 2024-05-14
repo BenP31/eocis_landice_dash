ebdd50f9aff7923fd02533b9e4c30a80e996c542
Ben Palmer <b.c.palmer@northumbria.ac.uk>
2024-05-14 15:01:13 +0100

    First commit
    kkk#	deleted:    processed_files/images/run032024/200605-201105.png

---
+++ app.py
@@ -0,0 +1,101 @@
+import os
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.set_page_config("EOCIS Land Ice dashboard", layout="wide")
+
+
+@st.cache_data
+def load_time_data(filename):
+    return pd.read_csv(filename)
+
+
+if "dash_dir" not in st.session_state:
+    st.session_state["dash_dir"] = os.environ.get("DASH_DIR", os.getcwd())
+
+
+timedata_df = load_time_data(
+    os.path.join(st.session_state["dash_dir"], "processed_files/time_series_data_2.csv")
+).sort_values("code")
+
+with st.sidebar:
+    periods = timedata_df["period"].to_list()
+    codes = timedata_df["code"].to_list()
+    time_period = st.selectbox(
+        "Time period",
+        options=range(len(periods)),
+        format_func=lambda x: periods[x],
+    )
+
+st.title(f"Surface elevation change for the period {periods[time_period]}")
+st.image(
+    os.path.join(st.session_state["dash_dir"], f"processed_files/images/{codes[time_period]}.png")
+)
+
+basin_selection = st.multiselect(
+    "Basins",
+    options=["all", *[str(i) for i in range(1, 28)]],
+    default=["all"],
+    max_selections=5,
+)
+
+max_v = np.round(timedata_df[basin_selection].abs().max(axis=None) + 0.05, 1)
+
+if len(basin_selection) != 0:
+    base = alt.Chart(timedata_df.reset_index(drop=True).reset_index())
+
+    nearest = alt.selection_point(nearest=True, on="mouseover", fields=["index"], empty=False)
+
+    line = base.mark_line().encode(
+        alt.X("index:Q", axis=alt.Axis(labels=False), title="Time Period"),
+        alt.Y(
+            alt.repeat("layer"),
+            type="quantitative",
+            scale=alt.Scale(domain=(-max_v, max_v)),
+            title="Mean elevation change (m/year)",
+        ),
+        color=alt.datum(alt.repeat("layer")),
+    )
+
+    # Draw a rule at the location of the selection
+    rules = (
+        base.mark_rule(color="gray")
+        .encode(
+            x="index:Q",
+            opacity=alt.condition(nearest, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("period", type="nominal", title="Time period"),
+            ]
+            + [alt.Tooltip(c, type="quantitative", format=".4f") for c in basin_selection],
+        )
+        .add_params(nearest)
+    )
+
+    points = line.mark_circle().encode(opacity=alt.condition(nearest, alt.value(1), alt.value(0)))
+
+    zero = pd.DataFrame([{"zero": 0.0}])
+    zero_rule = (
+        alt.Chart(zero)
+        .mark_rule(color="black", strokeDash=[1])
+        .encode(y="zero:Q", size=alt.value(1))
+    )
+
+    bar_chart = line + zero_rule + rules + points
+
+    bar_chart = bar_chart.repeat(layer=basin_selection)
+    
+    map = (
+    alt.Chart(
+        ais_basins.reset_index().to_crs("epsg:4326"),
+    )
+    .mark_geoshape()
+    .encode(color="basin_id:N", opacity=alt.condition(click_state, alt.value(1), alt.value(0.2)))
+    .project(type="stereographic")
+)
+
+    st.altair_chart(bar_chart.interactive(), use_container_width=True)
+else:
+    st.text("No data selected to plot")
---
+++ process_data.py
@@ -0,0 +1,180 @@
+import csv
+import os
+import re
+from glob import glob
+
+import cartopy.crs as ccrs
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+from netCDF4 import Dataset  # pylint:disable=no-name-in-module
+from shapely.geometry import Point
+
+file_years_re = re.compile("[0-9]{6}-[0-9]{6}")
+
+# Using IMBIE definitions, basin ids are 0-18
+csv_columns = ["code", "period", "all", *[str(i) for i in range(19)]]
+
+
+def get_data_files(folder: str):
+    for f in sorted(glob(os.path.join(folder, "*.nc"))):
+        yield f
+
+
+def plot_val(
+    gp_dataframe: gpd.GeoDataFrame,
+    shp_dataframe: gpd.GeoDataFrame,
+    plot_val,
+    x_values,
+    y_values,
+    extra=False,
+):
+    crs_3031 = ccrs.Stereographic(central_latitude=-90, true_scale_latitude=-71)
+
+    fig, ax = plt.subplots(
+        figsize=(9, 6),
+        facecolor="white",
+        subplot_kw=dict(projection=crs_3031),
+    )  # Create our plot
+
+    gp_dataframe.plot(
+        column=plot_val,
+        ax=ax,
+        legend=True,
+        vmax=2,
+        vmin=-2,
+        marker=".",
+        linewidths=0,
+        markersize=(fig.get_figwidth() * fig.get_dpi()) / len(x_values),
+        cmap="bwr_r",
+    )
+
+    shp_dataframe.plot(color="none", edgecolor="black", ax=ax, alpha=0.5, lw=0.7)
+    gl = ax.gridlines(draw_labels=True, color="black", alpha=0.25)
+    gl.ylabel_style = {
+        "color": "black",
+        "alpha": 0.5,
+    }
+
+    x_max, x_min = np.max(x_values.flatten()), np.min(x_values.flatten())
+    y_max, y_min = np.max(y_values.flatten()), np.min(y_values.flatten())
+    ax.set_xlim(x_min * 1.05, x_max * 1.05)
+    ax.set_ylim(y_min * 1.05, y_max * 1.05)
+
+    return fig
+
+
+def get_mean_data(gp_dataframe: gpd.GeoDataFrame, column_name: str) -> dict:
+    basin_means = (
+        gp_dataframe[gp_dataframe.notna()][[column_name, "basin_id"]]
+        .groupby("basin_id")
+        .mean()
+        .to_dict()[column_name]
+    )
+    basin_means["all"] = gp_dataframe[column_name].mean()
+    return basin_means
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser("EOCIS data processing script")
+    parser.add_argument(
+        "-x", "--aux_file_dir", help="Directory for auxillary files (shapefiles, etc.)"
+    )
+    parser.add_argument("-d", "--data_file_dir", help="Directory of input .nc files")
+    parser.add_argument(
+        "-p", "--processed_file_dir", help="Directory for processed files made here"
+    )
+    parser.add_argument(
+        "-t", "--time_series_file", help="Path to the csv file containing the time series data"
+    )
+
+    argv = parser.parse_args()
+
+    image_dir = argv.processed_file_dir
+    data_file_dir = argv.data_file_dir
+    time_series_file = argv.time_series_file
+    aux_file_dir = argv.aux_file_dir
+
+    ts_existing_entries = []
+    if os.path.exists(time_series_file):
+        with open(time_series_file, "r") as time_series_file_fp:
+            ts_reader = csv.reader(time_series_file_fp, delimiter=",")
+            for row in ts_reader:
+                ts_existing_entries.append(row[0])
+    else:
+        with open(time_series_file, "w+") as time_series_file_fp:
+            ts_writer = csv.DictWriter(time_series_file_fp, fieldnames=csv_columns, delimiter=",")
+            ts_writer.writeheader()
+
+    image_files = glob("*.png", root_dir=image_dir)
+
+    landice_files = get_data_files(data_file_dir)
+
+    for file_name in landice_files:
+        print("Processing ", file_name, end="", flush=True)
+        years = file_years_re.findall(file_name)
+        if len(years) == 0:
+            continue
+        file_year: str = years[0]
+        # 199107-199607
+        fmt_year = f"{file_year[0:4]}/{file_year[4:6]} - {file_year[7:11]}/{file_year[11:13]}"
+
+        if file_year in ts_existing_entries and file_year + ".png" in image_files:
+            print(" O O (Skipped)")
+            continue
+
+        # load shapefile
+        ais_basins = gpd.read_file(
+            os.path.join(aux_file_dir, "IMBIE_AIS_Basins", "ANT_Basins_IMBIE2_v1.6.shp")
+        )
+
+        # load nc
+        nc = Dataset(os.path.join(data_file_dir, file_name))
+        x_values = nc["x"][:].data
+        y_values = nc["y"][:].data
+        surf_type = nc["surface_type"][:].data
+        sec = nc["sec"][:].data[0, :, :]
+        basin_id = nc["basin_id"][:].data
+        basin_id[basin_id == -25] = 0
+
+        coords = []
+        for y in y_values:
+            for x in x_values:
+                coords.append(Point(x, y))
+
+        # make dataframe here
+        my_data = gpd.GeoDataFrame(
+            data={
+                "SEC": sec.flatten(),
+                "basin_id": basin_id.flatten(),
+                "surface_type": surf_type.flatten(),
+                "geometry": coords,
+            },
+            crs="epsg:3031",
+        )
+
+        if fmt_year not in ts_existing_entries:
+            # get mean data and add to csv
+            ts_data = get_mean_data(my_data, "SEC")
+            str_dict = {str(k): v for k, v in ts_data.items()}
+            ts_data = str_dict
+            ts_data.update({"code": file_year, "period": fmt_year})
+            with open(time_series_file, "a") as time_series_file_fp:
+                ts_writer = csv.DictWriter(
+                    time_series_file_fp, fieldnames=csv_columns, delimiter=","
+                )
+                ts_writer.writerow(ts_data)
+
+        print(" O", end="", flush=True)
+
+        if file_year + ".png" not in image_files:
+            # generate image and save in images folder
+            fig = plot_val(my_data[my_data.notna()], ais_basins, "SEC", x_values, y_values)
+            fig.savefig(os.path.join(image_dir, file_year + ".png"))
+            plt.close()
+
+        print(" O", end="", flush=True)
+
+        print("")
