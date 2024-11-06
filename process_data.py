15406a9a2dfa8ec5de062aaf23c821908aed8bf7
Ben Palmer <b.c.palmer@northumbria.ac.uk>
2024-11-06 13:52:10 +0000

    changed some things

--- process_data.py
+++ process_data.py
@@ -1,3 +1,4 @@
+import json
 import os
 import re
 import sys
@@ -18,6 +19,7 @@ file_years_re = re.compile("[0-9]{6}-[0-9]{6}")
 csv_columns = ["code", "period", "midpoint", "basin", "Raw SEC"]
 
 skip_basins = {0}
+skip_years = [str(y) for y in range(1991, 1996)]
 
 
 def get_data_files(folder: str):
@@ -41,6 +43,7 @@ def plot_antarc(
         column=plot_val,
         ax=ax,
         legend=True,
+        legend_kwds={"label": "Elevation Change (m/year)"},
         vmax=2,
         vmin=-2,
         marker=".",
@@ -85,6 +88,7 @@ def plot_greenl(
         column=plot_val,
         ax=ax,
         legend=True,
+        legend_kwds={"label": "Elevation Change (m/year)"},
         vmax=2,
         vmin=-2,
         marker="s",
@@ -98,13 +102,13 @@ def plot_greenl(
 
 
 def get_mean_data(gp_dataframe: gpd.GeoDataFrame, column_name: str) -> dict:
-    basin_means = (
+    basin_means: dict = (
         gp_dataframe[gp_dataframe.notna()][[column_name, "basin_id"]]
         .groupby("basin_id")
         .mean()
         .to_dict()[column_name]
     )
-    basin_means["all"] = gp_dataframe[column_name].mean()
+    basin_means["All"] = gp_dataframe[column_name].mean()
     return basin_means
 
 
@@ -114,6 +118,23 @@ def time_to_ts(time):
     seconds_in_year = (end_of_year - start_of_year).total_seconds()
 
     return datetime(year=int(time // 1), month=1, day=1).timestamp() + time % 1 * seconds_in_year
+
+
+def load_json_file(filepath: str) -> dict:
+    if not os.path.exists(filepath):
+        return {}
+
+    with open(filepath, "r") as fp:
+        my_json = json.load(fp)
+        return {
+            x["id"]: {"fileName": x["fileName"], "displayName": x["displayName"]} for x in my_json
+        }
+
+
+def save_json_file(filepath: str, data: dict) -> None:
+    out_json = [{"id": k} | v for k, v in data.items()]
+    with open(filepath, "+w") as fp:
+        json.dump(out_json, fp)
 
 
 if __name__ == "__main__":
@@ -137,7 +158,8 @@ if __name__ == "__main__":
     data_file_dir = argv.data_file_dir
     aux_file_dir = argv.aux_file_dir
 
-    image_files = glob("*.png", root_dir=image_dir)
+    image_json_path = os.path.join(argv.processed_file_dir, f"{argv.area}_images.json")
+    image_files = load_json_file(image_json_path)
 
     landice_files = get_data_files(data_file_dir)
 
@@ -148,124 +170,143 @@ if __name__ == "__main__":
         )
         crs = "epsg:3031"
         time_series_file = os.path.join(argv.processed_file_dir, "time_series_data_AIS.csv")
+        basin_file = "aux_files/ais_basins.json"
     elif arg_area == "GrIS":
         basin_df = gpd.read_file(
             os.path.join(aux_file_dir, "IMBIE_GIS_Basins", "Greenland_Basins_PS_v1.4.2.shp")
         )
         crs = "epsg:3413"
-        time_series_file = os.path.join(argv.processed_file_dir, "time_series_data_GIS.csv")
+        time_series_file = os.path.join(argv.processed_file_dir, "time_series_data_GrIS.csv")
+        basin_file = "aux_files/gris_basins.json"
     else:
         sys.exit("Invalid area selection")
 
     all_data = []
 
-    try:
-        for file_name in landice_files:
-            print("Processing", file_name, flush=True)
+    for file_name in landice_files:
+        file_dates_search = file_years_re.findall(file_name)
+        if len(file_dates_search) == 0:
+            continue
+        # 199107-199607
+        file_year: str = file_dates_search[0]
 
-            file_dates_search = file_years_re.findall(file_name)
-            if len(file_dates_search) == 0:
-                continue
-            file_year: str = file_dates_search[0]
-            # 199107-199607
-            fmt_year = f"{file_year[0:4]}/{file_year[4:6]} - {file_year[7:11]}/{file_year[11:13]}"
-            code_year = f"{file_year[0:4]}-{file_year[7:11]}-{file_year[4:6]}"
+        # skip period if beginning year is in skip_year
+        # "I think for Greenland and Antarctica land ice it would be best if we start the time
+        # series from 1996 onwards. There are known issues with the data before then and these are
+        # particularly obvious in the Antarctica dashboard."
+        if file_year[:4] in skip_years:
+            print("Skipping ", file_name, "...", sep="")
+            continue
 
-            # load nc
-            nc = Dataset(file_name)
-            surf_type = nc["surface_type"][:].data
-            sec = nc["sec"][:].data[0, :, :]
-            basin_id = nc["basin_id"][:].data
+        fmt_year = f"{file_year[0:4]}/{file_year[4:6]} - {file_year[7:11]}/{file_year[11:13]}"
+        code_year = f"{file_year[0:4]}-{file_year[7:11]}-{file_year[4:6]}"
 
-            # Error in basin number for AIS
-            if arg_area == "AIS":
-                basin_id[basin_id == -25] = 0
+        # load nc
+        print("Processing", file_name, flush=True)
+        nc = Dataset(file_name)
+        sec = nc["sec"][:].data[0, :, :]
+        basin_id = nc["basin_id"][:].data
 
+        # Error in basin number for AIS
+        if arg_area == "AIS":
+            basin_id[basin_id == -25] = 0
 
-            # make dataframe here
-            if file_year + ".png" not in image_files:
-                x_values = nc["x"][:].data
-                y_values = nc["y"][:].data
-                x_coords, y_coords = np.meshgrid(x_values, y_values, indexing="xy")
-                coords_arr = [Point(x, y) for x, y in zip(x_coords.flatten(), y_coords.flatten())]
-                my_data = gpd.GeoDataFrame(
-                    data={
-                        "Raw SEC": sec.flatten(),
-                        "basin_id": basin_id.flatten(),
-                        "surface_type": surf_type.flatten(),
-                        "geometry": coords_arr,
-                    },
-                    crs=crs,
-                )
-            else:
-                my_data = pd.DataFrame(
-                    data={
-                        "Raw SEC": sec.flatten(),
-                        "basin_id": basin_id.flatten(),
-                    }
-                )
-
-            ts_data = get_mean_data(my_data, "Raw SEC")
-            midpoint = datetime.fromtimestamp(
-                (time_to_ts(nc["start_time"][:].data[0]) + time_to_ts(nc["end_time"][:].data[0]))
-                / 2
+        # make dataframe here
+        if file_year + ".png" not in image_files:
+            x_values = nc["x"][:].data
+            y_values = nc["y"][:].data
+            x_coords, y_coords = np.meshgrid(x_values, y_values, indexing="xy")
+            coords_arr = [Point(x, y) for x, y in zip(x_coords.flatten(), y_coords.flatten())]
+            my_data = gpd.GeoDataFrame(
+                data={
+                    "Raw SEC": sec.flatten(),
+                    "basin_id": basin_id.flatten(),
+                    "geometry": coords_arr,
+                },
+                crs=crs,
+            )
+        else:
+            my_data = pd.DataFrame(
+                data={
+                    "Raw SEC": sec.flatten(),
+                    "basin_id": basin_id.flatten(),
+                }
             )
 
-            for k, v in ts_data.items():
-                if k in skip_basins:
-                    continue
+        ts_data = get_mean_data(my_data, "Raw SEC")
+        midpoint = datetime.fromtimestamp(
+            (time_to_ts(nc["start_time"][:].data[0]) + time_to_ts(nc["end_time"][:].data[0])) / 2
+        )
 
-                all_data.append(
-                    {
-                        "code": code_year,
-                        "period": fmt_year,
-                        "midpoint": midpoint,
-                        "basin": k,
-                        "Raw SEC": v,
-                    }
-                )
+        for k, v in ts_data.items():
+            if k in skip_basins:
+                continue
 
-            if file_year + ".png" not in image_files:
-                # generate image and save in images folder
-                if arg_area == "AIS":
-                    fig = plot_antarc(
-                        my_data[my_data.notna()], basin_df, "Raw SEC", x_values, y_values
-                    )
-                elif arg_area == "GrIS":
-                    fig = plot_greenl(
-                        my_data[my_data.notna()], basin_df, "Raw SEC", x_values, y_values
-                    )
-                else:
-                    sys.exit("Invalid area selection")
-                fig_file_path = os.path.join(image_dir, file_year + ".png")
-                print(f"Saving figure to {fig_file_path}")
-                fig.savefig(fig_file_path)
-                plt.close()
+            all_data.append(
+                {
+                    "period": fmt_year,
+                    "midpoint": midpoint,
+                    "basin": k,
+                    "Raw SEC": v,
+                }
+            )
 
-    except KeyboardInterrupt as e:
-        print("\nRecieved KeyboardInterrupt" + str(e))
-    finally:
-        # print all data to csv file
-        # keyboard interrupt still writes all collected data
-        df = pd.DataFrame(all_data)
-        df["Smooth SEC"] = np.zeros(len(df))
-        df["dH"] = np.zeros(len(df))
+        if code_year not in image_files:
+            # generate image and save in images folder
+            if arg_area == "AIS":
+                fig = plot_antarc(my_data[my_data.notna()], basin_df, "Raw SEC", x_values, y_values)
+            elif arg_area == "GrIS":
+                fig = plot_greenl(my_data[my_data.notna()], basin_df, "Raw SEC", x_values, y_values)
+            else:
+                sys.exit("Invalid area selection")
+            fig_file_path = os.path.join(image_dir, code_year + ".png")
+            print(f"Saving figure to {fig_file_path}")
+            fig.savefig(fig_file_path)
+            image_files.update(
+                {code_year: {"fileName": code_year + ".png", "displayName": fmt_year}}
+            )
+            plt.close()
 
-        for basin_no in df["basin"].unique():
-            basin_sec = df[df["basin"] == basin_no]["Raw SEC"]
-            smooth_basins = uniform_filter1d(basin_sec, size=5)
-            df.loc[df["basin"] == basin_no, "Smooth SEC"] = smooth_basins
+            del fmt_year
 
-            dh_values = np.zeros(len(basin_sec))
+    # print all data to csv file
+    # keyboard interrupt still writes all collected data
+    timeseries_df = pd.DataFrame(all_data)
+    timeseries_df["Smooth SEC"] = np.zeros(len(timeseries_df))
+    timeseries_df["dH"] = np.zeros(len(timeseries_df))
 
-            for i in range(len(basin_sec)):
-                dh_values[i] = np.trapz(basin_sec[: i + 1])
+    for basin_no in timeseries_df["basin"].unique():
+        basin_sec = timeseries_df[timeseries_df["basin"] == basin_no]["Raw SEC"]
+        smooth_basins = uniform_filter1d(basin_sec, size=5)
+        timeseries_df.loc[timeseries_df["basin"] == basin_no, "Smooth SEC"] = smooth_basins
 
-            df.loc[df["basin"] == basin_no, "dH"] = dh_values
+        dh_values = np.zeros(len(basin_sec))
 
-        df["Raw SEC"] = df["Raw SEC"].round(3)
-        df["Smooth SEC"] = df["Smooth SEC"].round(3)
+        for i in range(len(basin_sec)):
+            dh_values[i] = np.trapz(basin_sec[: i + 1])
 
-        # save dataframe
-        print("Saving data to", time_series_file)
-        df.to_csv(time_series_file, sep=",", index=False)
+        timeseries_df.loc[timeseries_df["basin"] == basin_no, "dH"] = dh_values
+
+    timeseries_df["Raw SEC"] = timeseries_df["Raw SEC"].round(3)
+    timeseries_df["Smooth SEC"] = timeseries_df["Smooth SEC"].round(3)
+
+    # add basin names to dataframe
+
+    basin_df = gpd.GeoDataFrame.from_file(basin_file)  # load basin file
+
+    timeseries_df["basin"] = timeseries_df["basin"].astype(str)
+    timeseries_df = pd.merge(
+        timeseries_df,
+        basin_df[["basin_id", "Subregion"]],
+        how="outer",
+        left_on="basin",
+        right_on="basin_id",
+    )
+    timeseries_df.loc[timeseries_df["basin"] == "All", "Subregion"] = "All"
+    timeseries_df = timeseries_df.drop(columns=["basin", "basin_id"])
+
+    # save dataframe
+    print("Saving data to", time_series_file)
+    timeseries_df.to_csv(time_series_file, sep=",", index=False)
+
+    save_json_file(image_json_path, image_files)
